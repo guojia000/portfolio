b6e9d31aee1346837e39a51588a3f3772b42db5b
guojia000 <144977652+guojia000@users.noreply.github.com>
2023-09-14 23:39:26 +1000

    update js file

--- js/script.js
+++ js/script.js
@@ -16,3 +16,36 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('active');
 };
+
+function my(id){
+    return document.getElementById(id);
+}
+my("More-btn-edu").onclick=function(){
+    if(this.value == "Show More") {
+        my("hide-edu").style.display="Block";
+        this.value="Show Less";
+    } else if(this.value == "Show Less") {
+        my("hide-edu").style.display="None";
+        this.value="Show More";
+    }
+}
+my("More-btn-skl").onclick=function(){
+    if(this.value == "Show More") {
+        my("hide-skl").style.display="Block";
+        this.value="Show Less";
+    } else if(this.value == "Show Less") {
+        my("hide-skl").style.display="None";
+        this.value="Show More";
+    }
+}
+my("More-btn-cert").onclick=function(){
+    if(this.value == "Show More") {
+        my("hide-cert").style.display="Block";
+        this.value="Show Less";
+    } else if(this.value == "Show Less") {
+        my("hide-cert").style.display="None";
+        this.value="Show More";
+    }
+}
+
+
